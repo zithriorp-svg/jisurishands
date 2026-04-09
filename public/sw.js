15832022b0af4d458ed3ff6fc64bd456853563ba
zithriorp-svg <zithriorp@gmail.com>
2026-04-09 16:56:57 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,11 @@
-self.addEventListener('fetch', function(event) {});
-
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => new Response('Vault Offline Mode Active'))
+  );
+});
